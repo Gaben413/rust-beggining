2543114ea4577d8942d850f8c4e7ba86af7bc6c5
NowhereCat <gabrielbportela413@gmail.com>
2023-12-17 12:53:12 -0300

    Update main.rs

--- tutorial1/src/main.rs
+++ tutorial1/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 4;
+    println!("x is: {}", x);
+
+    let x = "hello";
+    println!("x is: {}", x);
+
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
 }
