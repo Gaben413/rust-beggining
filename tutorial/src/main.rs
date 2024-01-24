cb371938a177c83470055f1d2aaa53eea1119587
Gaben <gabrielbportela413@gmail.com>
2024-01-23 21:35:07 -0300

    Update main.rs

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    let tup: (i32, bool, char) = (1, true, 's');
-    println!("{}", tup.0)
-    
+    let mut tup: (i32, bool, char) = (1, true, 's');
+
+    tup.0 = 10;
+    tup = (15, false, 'w');
+
+    println!("{}", tup.0);
+
+    let mut arr: [i32; 5] = [1,2,3,4,5];
+    arr[0] = 3;
+    println!("{}", arr[4]);
 }
