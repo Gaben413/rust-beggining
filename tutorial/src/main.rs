c2ec8964adcbb98b61530e8ef3cba68fabb099ab
Gaben <gabrielbportela413@gmail.com>
2024-01-23 23:05:50 -0300

    Update main.rs

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("expected to read line");
+
+    let int_input: i64 = input.trim().parse().unwrap();
+
+    println!("{}", int_input+2);
 }
